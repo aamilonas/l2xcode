b78091f0d85749c0731594af797cc210115df747
Angelo <54587354+aamilonas@users.noreply.github.com>
2025-04-14 07:09:54 -0400

    all done

--- L2 Demo/ContentView.swift
+++ L2 Demo/ContentView.swift
@@ -9,14 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        
+        ZStack{
+            Color(.systemMint)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20.0){
+                Image("Tree")
+                    .resizable()
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fit/*@END_MENU_TOKEN@*/)
+                    .cornerRadius(15)
+                
+                HStack {
+                    Text("Tree")
+                        .font(.title)
+                        .fontWeight(.bold)
+                    
+                    Spacer()
+                    
+                    VStack{
+                        HStack{
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                        }
+                        Text("(Reviews 361)")
+                    }.foregroundColor(.orange)
+                        .font(.caption)
+                }
+                
+                
+                Text("Come see the tree it is fully erect and anticipating a visit.")
+                
+                HStack{
+                    Spacer()
+                    Image(systemName: "binoculars.fill")
+                    Image(systemName: "fork.knife")
+                }.foregroundColor(.gray)
+                    .font(.caption)
+            }
+            .padding()
+            .background(Rectangle().foregroundColor(.white))
+            .cornerRadius(15)
+            .shadow(radius: 15)
+            .padding()
         }
-        .padding()
     }
+        
 }
 
 #Preview {
